256e8ec1dd9f300dea181e813a8fab712841d5be
Velorian-TDV <terleckydima92@gmail.com>
2023-06-02 16:05:27 +0300

    First task complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+startButton.addEventListener('click', () => {
+
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+});
+
+stopButton.addEventListener('click', () => {
+
+    clearInterval(intervalId);
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+});
