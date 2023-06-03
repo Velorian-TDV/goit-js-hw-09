338e068ed39a780176036f9619f54359faaa9995
Velorian-TDV <terleckydima92@gmail.com>
2023-06-03 22:57:14 +0300

    Third task complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from "notiflix";
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector(`input[name='delay']`);
+const stepInput = document.querySelector(`input[name='step']`);
+const amountInput = document.querySelector(`input[name='amount']`);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+    
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });    
+            }
+        }, delay);
+    
+    });
+
 }
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    const delay = Number(delayInput.value);
+    const step = Number(stepInput.value);
+    const amount = Number(amountInput.value);
+
+    let timeIncrement = delay;
+
+    for (let amountIncrement = 1; amountIncrement <= amount; amountIncrement++) {
+        createPromise(amountIncrement, timeIncrement)
+            .then(data => {
+                Notify.success(`Fulfilled promise ${data.position} in ${data.delay}ms`);
+            })
+            .catch(err => {
+                Notify.failure(`Rejected promise ${err.position} in ${err.delay}ms`);
+            });
+
+        timeIncrement += step;
+    }
+
+});
